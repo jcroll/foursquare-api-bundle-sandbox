e260173c9144609de3e9be50aa31bd1b30ab8411
Jarrett Croll <jarrett.croll@gmail.com>
2013-07-11 14:35:45 +0000

    attempting to integrate oauth

--- src/Application/DefaultBundle/Controller/DefaultController.php
+++ src/Application/DefaultBundle/Controller/DefaultController.php
@@ -11,7 +11,10 @@ class DefaultController extends Controller
     {
         /** @var $client FoursquareClient */
         $client = $this->get('jcroll_foursquare_client');
-        $command = $client->getCommand('GetVenuesCategories');
+        $command = $client->getCommand('venues/search', array(
+            'near' => 'Chicago, IL',
+            'query' => 'sushi'
+        ));
         ladybug_dump_die($command->execute());
 
         return $this->render('ApplicationDefaultBundle:Default:index.html.twig', array('name' => $name));
